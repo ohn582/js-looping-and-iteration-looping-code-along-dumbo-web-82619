f0bf363c8c923bab059bd886c27303352cfa797e
Neal JIn Oh <no4259@gmail.com>
2019-10-27 04:11:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(name, event){
+  for (let i = 0; i < 3; i += 1){
+    console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+  }
+}
+
+writeCards(name, event);
+
+function countDown(num){
+  for (let i = 4; 0 > i; i -= 1){
+    console.log(i)
+  }
+}
